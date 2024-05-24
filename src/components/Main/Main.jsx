95ae3d986c0b78f2155e180a4e4b1543380cebff
adarsh7123 <adarshverma127@gmail.com>
2024-05-24 19:44:08 +0530

    final ui of gemini completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,15 +5,55 @@ import { assets } from '../../assets/assets'
 function Main() {
 
 
-  return (
-    <div className="main">
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
+    return (
+        <div className="main">
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
 
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Developer.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p> Is HTML a programming Language,Explain it!</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>What are some necessary skills to improve yourself?</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Add some comments to the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a Promt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Gemini may display inaccurate information, including about peopel, so double-check its responses. Your privacy and Gemini Apps.
+                    </p>
+                </div>
+
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Main
